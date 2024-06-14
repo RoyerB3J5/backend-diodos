7974113960fa16bb630aceea04806b762e10e616
Royer Bonifacio <royer.boni13@gmail.com>
2024-06-14 14:45:57 -0500

    change footer 1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,9 +17,7 @@ function Home() {
       <Somos />
       
       <Form />
-      <footer className="w-full  mt-10 px-10 sm:px-32 py-5 flex items-center justify-around gap-x-3 text-center text-gray-300 ">
-        <p>Copyright &copy; - UNT Ing. Mecatrónica - RRMIC</p>
-      </footer>
+      
     </>
 
   )
